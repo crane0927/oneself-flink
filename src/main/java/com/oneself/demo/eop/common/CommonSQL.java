6d0dc4759fe97f3cd9007da3cdfa4205c57ca7ca
liuhuan <crane0927@163.com>
2025-03-12 11:43:17 +0800

    refactor(eop): 精简通用 SQL 字段
    - 注释掉部分不必要的字段，包括 appSource、appName、apiSource、apiName、apiInsId、apiVersion

--- src/main/java/com/oneself/demo/eop/common/CommonSQL.java
+++ src/main/java/com/oneself/demo/eop/common/CommonSQL.java
@@ -14,36 +14,36 @@ public class CommonSQL {
      * 通用建表字段
      */
     public static final String COMMON_CREATE_ROWS = "  appId STRING, \n" + // 应用调用者 ID
-            "  appSource INT, \n" + // 应用调用者 ID (整数)
-            "  appName STRING, \n" + // 应用调用者名称
-            "  apiId STRING, \n" + // API 提供方 ID
-            "  apiSource INT, \n" + // API 来源 (整数)
-            "  apiName STRING, \n" + // API 名称
-            "  apiInsId STRING, \n" + // API 内部实例 ID
-            "  apiVersion STRING, \n" + // API 版本
+//            "  appSource INT, \n" + // 应用调用者 ID (整数)
+//            "  appName STRING, \n" + // 应用调用者名称
+            "  apiId STRING,"; // API 提供方 ID
+//            "  apiSource INT, \n" + // API 来源 (整数)
+//            "  apiName STRING, \n" + // API 名称
+//            "  apiInsId STRING, \n" + // API 内部实例 ID
+//            "  apiVersion STRING, \n" + // API 版本
 //                "  gatewayHost STRING, \n" + // 网关地址
 //                "  targetHost STRING, \n" + // 目标路由地址
-            "  path STRING, \n" + // 请求路径
-            "  host STRING, \n" + // 请求 HOST
-            "  providerName STRING, \n" + // 提供者名称
-            "  consumerName STRING, \n"; // 消费者名称 (字符串)
+//            "  path STRING, \n" + // 请求路径
+//            "  host STRING, \n" + // 请求 HOST
+//            "  providerName STRING, \n" + // 提供者名称
+//            "  consumerName STRING, \n"; // 消费者名称 (字符串)
 
 
     /**
      * 通用分组字段
      */
     public static final String COMMON_GROUP_ROWS = "  appId,\n" +
-            "  appSource,\n" +
-            "  appName,\n" +
-            "  apiId,\n" +
-            "  apiSource,\n" +
-            "  apiName,\n" +
-            "  apiInsId,\n" +
-            "  apiVersion,\n" +
+//            "  appSource,\n" +
+//            "  appName,\n" +
+            "  apiId,";
+//            "  apiSource,\n" +
+//            "  apiName,\n" +
+//            "  apiInsId,\n" +
+//            "  apiVersion,\n" +
 //                "  gatewayHost,\n" +
 //                "  targetHost,\n" +
-            "  path,\n" +
-            "  host,\n" +
-            "  providerName,\n" +
-            "  consumerName,\n";
+//            "  path,\n" +
+//            "  host,\n" +
+//            "  providerName,\n" +
+//            "  consumerName,\n";
 }
